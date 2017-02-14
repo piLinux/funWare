6e77dc81af0a1dabde2ab3d8bca4602f8da66ad4
Mahir Hasan <mahirhasan@live.com>
2017-02-14 02:04:22 +0100

    Create Schematic1_Solution2.c

---
+++ 003. Traffic Light/Schematic1_Solution2.c
@@ -0,0 +1,26 @@
+/*
+ * trafficLight.c
+ *
+ * Created: 02/12/2017 12:36:50 PM
+ * Author : mahir
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRD = 0xff;	// Set all Port D pins as outputs
+	
+	while(1)
+	{
+		PORTD = (1 << PD2);
+		_delay_ms(1000);
+		PORTD = (1 << PD3);
+		_delay_ms(1000);
+		PORTD = (1 << PD4);
+		_delay_ms(1000);		
+	}
+  
+    return 0;
+}
